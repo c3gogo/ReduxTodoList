240e60d22dee3037173e7569560764ac6a2ee768
corentingogo <corentin.gogo@wanadoo.fr>
2018-01-11 21:38:58 +0100

    AddForm component and action creator

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import AddForm from './add_form';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AddForm />
+      </div>
+    );
+  }
+}
+
+export default App;
